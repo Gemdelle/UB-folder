5edae25cb2ac178da6716c061dc57390acb5cec0
Gemdelle <milu200911@hotmail.com>
2024-05-16 20:15:57 -0300

    Files.

---
+++ ingeniería-de-software/app-mockup/script.js
@@ -0,0 +1,6 @@
+function mostrarOtraPantalla() {
+    // Oculta el login-screen
+    document.getElementById('login-screen').style.display = 'none';
+    // Muestra la otra pantalla
+    document.getElementById('otra-pantalla').style.display = 'block';
+}
